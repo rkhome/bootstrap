ea2615741a25afd5a30aa9aa9553ff7546d33401
Raghvendra <raghvendra@ubuntu.ubuntu-domain>
2012-07-08 01:45:51 +0530

    appliyed boot strap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UseBootstrap::Application.config.secret_token = '276e259d74d8ddaac393ae39a49e80772a8ad49e86051998065e2aa7efac9d0ca91ca1611a2b7e1d109506f7f507ca03e1014131df79c0ef75d41b255b7d7e89'
